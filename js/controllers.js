3b0c8ccbf682c2e44acebd168826f4b48ba80b6e
UlyssesLin <utemoc@gmail.com>
2015-03-12 02:44:50 -0700

    2:44am 3/12: projects rehaul with thumbnails

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,15 @@
 portfolio.controller('Skills',function($scope){
-	$scope.lamp_list = ['Linux','Ajax','MySQL','PHP'];
-	$scope.mean_list = ['MongoDB + Mongoose','Express.js/Socket.io','Angular.js','Node.js'];
-	$scope.rails_list = ['SQLite','WEBrick','+ gem creation'];
-	$scope.back_list = ['OOP','MVC','TDD','Chai','Rspec','Capybara','ORM','RESTful'];
-	$scope.front_list = ['HTML5','CSS3','jQuery','jQuery UI','RWD','LESS','SASS','HAML','Skeleton','Bootstrap'];
+	$scope.lamp_list = ['Ajax','MySQL','CodeIgniter'];
+	$scope.mean_list = ['MongoDB','Mongoose','Express.js','AngularJS','Node.js','Socket.io'];
+	$scope.rails_list = ['SQLite','WEBrick','Gem Creation'];
+	$scope.concepts_list = ['OOP','MVC','TDD','ORM','RESTful'];
+	$scope.front_list = ['HTML5','CSS3','jQuery','jQuery UI','Responsive Web Design','LESS','SASS','HAML','Skeleton','Bootstrap'];
 	$scope.tools_list = ['Git + GitHub','APIs','PhotoShop','Illustrator','InDesign','Balsamiq'];
+});
+portfolio.controller('Projects',function($scope){
+	$scope.image=0;
+	$scope.n=0;
+	$scope.change_image=function(n){
+		$scope.image=n;
+	}
 });
